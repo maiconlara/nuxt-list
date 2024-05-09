4dd1d00ac1849bbbd8baefc06a1a31324cfe7c8f
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-09 14:48:52 -0300

    setup nuxt app

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-full h-screen bg-default text-white">
+        <NuxtLayout >
+           <NuxtPage />  
+        </NuxtLayout>
+    </div>
+</template>
---
+++ pages/hello.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="text-title font-bold">Hello </h1>
+  <div
+    class="flex flex-col items-center justify-center w-full h-[100px] bg-accent"
+  >
+    <h1>World</h1>
+  </div>
+</template>
