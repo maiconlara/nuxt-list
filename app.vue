50c7c2838a9d19aeb57298c0ef530d5aa14c094b
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-13 15:35:24 -0300

    feat: adding color mode button component

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="flex flex-col items-center justify-center w-full h-screen bg-default text-white font-nunito">
-        <NuxtLayout >
-           <NuxtPage />  
-        </NuxtLayout>
-    </div>
+  <div
+    class="flex flex-col items-center justify-center w-full h-screen bg-white dark:bg-blue-900 text-black font-nunito"
+  >
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
